a285a43988245bc3bd4c56f5057302df21cc2555
Christophe Blaess <christophe.blaess@logilin.fr>
2020-07-01 06:45:51 +0200

    [DEV] Add GPL header in source file.

--- run-as.c
+++ run-as.c
@@ -1,3 +1,23 @@
+/*
+ * Small tool to run command as non-priviledged user inboot script.
+ *
+ * (c) 2020 Christophe BLAESS <christophe.blaess@logilin.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
 #include <grp.h>
 #include <pwd.h>
 #include <stdio.h>
@@ -40,10 +60,12 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "%s: unable to set G-ID.\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+
 	if (setreuid(uid, uid) != 0) {
 		fprintf(stderr, "%s: unable to set U-ID.\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+
 	return system(argv[3]);
 }
 
